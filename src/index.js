5a6a54c489345dc2a32c34ea635b34e368b7038e
kwizl <karis.njoroge@gmail.com>
2021-01-04 11:48:45 +0300

    Implement Redux Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
 );
 
 reportWebVitals();
